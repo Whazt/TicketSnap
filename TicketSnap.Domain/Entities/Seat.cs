6abbabd70ccfa834210fe5ee85e8bf41e2882204
whazt <walddt05@gmail.com>
2026-02-09 22:24:33 -0600

    feat: define seat domain model with status enum

---
+++ TicketSnap.Domain/Entities/Seat.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicketSnap.Domain.Enums;
+
+namespace TicketSnap.Domain.Entities
+{
+    public class Seat
+    {
+        public int Id { get; set; }
+        public string Row { get; set; } = string.Empty;
+        public int Number { get; set; }
+        public decimal Price { get; set; }
+        public DateTime? LockedUntil { get; set; }
+        public Guid? ReservedByUser { get; set; }
+        public SeatStatus Status { get; set; } = SeatStatus.Available;
+    }
+}
---
+++ TicketSnap.Domain/Enums/SeatStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketSnap.Domain.Enums
+{
+    public enum SeatStatus
+    {
+        Available = 1,
+        Pending = 2,
+        Sold = 3
+    }
+}
