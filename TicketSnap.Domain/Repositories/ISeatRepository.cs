4e38e2bae5af21c16626743b676f48db518aadd5
whazt <walddt05@gmail.com>
2026-02-09 22:30:14 -0600

    feat: add seat repository interface

---
+++ TicketSnap.Domain/Repositories/ISeatRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicketSnap.Domain.Entities;
+
+namespace TicketSnap.Domain.Repositories
+{
+    public interface ISeatRepository
+    {
+        Task<Seat> GetSeatByIdAsync(int seatId);
+        Task<IEnumerable<Seat>> GetAllAsync(int eventId);
+        Task UpdateSeatAsync(Seat seat);
+        Task<bool> TryLockSeatAsync(int seatId, Guid userId, DateTime expiration);
+
+    }
+}
